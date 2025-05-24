e06c91a9a6654ce4000c6b9d70f6ad322ae11926
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-05-24 15:35:30 +0400

    Add dataset download scripts for various configurations
    This commit introduces three new bash scripts (`run_1.sh`, `run_2.sh`, `run_3.sh`) to download datasets for different configurations: RN50, Delta CLIP Vit L 14, and Downstream. Each script fetches the respective dataset archives from Dropbox links. These scripts aim to streamline the dataset preparation process for different model settings.

---
+++ downloading_datasets/run_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#RN50
+
+
+https://www.dropbox.com/scl/fi/skewhyd1geb4vontgjetu/Aircraft.tar.gz?rlkey=9fs4q11et7pq78uaaor35qv3r&st=61wbvy24&dl=0
+wget https://www.dropbox.com/scl/fi/aejuk0rpk7plow6o93cu9/Caltech101.tar.gz?rlkey=650ns21azi1p9sphh54tdw63b&st=fqmplhd1&dl=0
+wget https://www.dropbox.com/scl/fi/sddxl0x0sl33xs5qpftha/Cars.tar.gz?rlkey=k3dkvdk4ngxt1jcykywaqdn4x&st=izxbuh78&dl=0
+wget https://www.dropbox.com/scl/fi/1yo195cdjc2u36d5z3olr/DTD.tar.gz?rlkey=qvfppj9hbnq3w6y7ewjrkm95t&st=aaagst6v&dl=0
+wget https://www.dropbox.com/scl/fi/64ehcilq4mxtinf1f4e8b/eurosat.tar.gz?rlkey=wkh9yyeurm27qczw68p522d69&st=p8sj08gs&dl=0
+wget https://www.dropbox.com/scl/fi/pzhoyargzmgl7j2pjkkr8/Flower102.tar.gz?rlkey=ikmulqy0q7lj0kjc2dgo0z3hu&st=ks7046km&dl=0
+wget https://www.dropbox.com/scl/fi/cx6vm3o64cnrj5bkq753e/Pets.tar.gz?rlkey=qn44gr7idhfay75xyucj4gf0d&st=tb9gyeku&dl=0
+wget https://www.dropbox.com/scl/fi/5ufk185uvvnwuxhjaboc2/UCF101.tar.gz?rlkey=wmg76x6pik8jadk5kjqkq7e33&st=yn9umgnb&dl=0
+
+
+
---
+++ downloading_datasets/run_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Delta CLIP Vit L 14
+wget https://www.dropbox.com/scl/fi/9ht15xqbq210e7z035f8s/Aircraft.tar.gz?rlkey=28mz40niaoukrdqfwslknzset&st=lty62a8z&dl=0
+wget https://www.dropbox.com/scl/fi/x7ycpahyz30e7e9vbib4t/Caltech101.tar?rlkey=wfypfpt2zd1vztp3timpy8f8v&st=b5m8ev5s&dl=0
+wget https://www.dropbox.com/scl/fi/nlhi7z5e827u39160k6ao/Cars.tar.gz?rlkey=go34x1dpi79m6s4fx7ccw55wx&st=ol75vrzb&dl=0
+wget https://www.dropbox.com/scl/fi/bvmp6i57hfxeyfpbetnzi/DTD.tar.gz?rlkey=tyw3sbt0ow4si2yqztnhqntrk&st=km1yijpk&dl=0
+wget https://www.dropbox.com/scl/fi/swocq98i1e5w4mer1zt99/eurosat.tar.gz?rlkey=rfd3eqkivw299vir6958cq2zj&st=1ped7wta&dl=0
+wget https://www.dropbox.com/scl/fi/txym07e7xglcq1nftisxm/Flower102.tar.gz?rlkey=l13b6ql15e8x2d3bhj07g9edl&st=gl5dl4h6&dl=0
+wget https://www.dropbox.com/scl/fi/knni3jvvdnmwol96aq7w3/Pets.tar.gz?rlkey=fmafbiti7tgsehlgq1rytjze0&st=9j69ch1r&dl=0
+wget https://www.dropbox.com/scl/fi/zbbojwntjnuoqis39u32f/UCF101.tar.gz?rlkey=ao7fkrb91t7o5nsuqzzrxv8v7&st=seinobhl&dl=0
