dfc22e54778555c0501d8079eedf7d681d7c2609
Naufal Suryanto <51362638+naufalso@users.noreply.github.com>
2025-05-30 10:14:01 +0400

    Fix output dir script_to_run.sh

--- scripts/script_to_run.sh
+++ scripts/script_to_run.sh
@@ -5,7 +5,7 @@ LAUNCHER="bash scripts/train_cluster_fg.sh"
 ATPT_DATASET="/leonardo_work/EUHPC_R04_192/fmohamma/Adversarial_Robust_Clip/atpt_data"
 DOWNSTREAM_DATASET="$ATPT_DATASET/downstream_DOWNSTREAM_DATASETs"
 MODEL="ViT-L/14"
-OUTPUT_DIR="$OUTPUT_DIR"
+OUTPUT_DIR="/leonardo_work/EUHPC_R04_192/fmohamma/Adversarial_Robust_Clip/ATPT/Final_Results"
 
 sbatch run_1d.sh "$LAUNCHER $DOWNSTREAM_DATASET  0 4 $MODEL 4.0 100 0 0.1 20 0.01 rtpt none $ATPT_DATASET true pgd 2 4.0 1.0 0.2 2.0 true $OUTPUT_DIR"
 
