1e2ae8db9fd4cc033e55463bbfe3c85ee9a8f2d5
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-06-03 11:17:21 +0400

    `Refactor job script to streamline model evaluation and add job completion wait function`
    ```markdown
    # Refactor job script
    ### language: bash
    - cd /leonardo_work/EUHPC_R04_192/fmohamma/Adversarial_Robust_Clip/atpt_data
    - mkdir vit_l_14_datacomp_1b
    - wget https://www.dropbox.com/scl/fi/cnn70dahaftyyv2n78w1j/Aircraft.zip?rlkey=e9y0g3d8cs5c5rinphjqt41aw&st=rw0gcj1h&dl=0
    - wget https://www.dropbox.com/scl/fi/s1917wxk72afma8lg7vdq/Caltech101.zip?rlkey=hk0gmdzqpxnsp0hz2mk65byvt&st=97m7ycrh&dl=0
    - wget https://www.dropbox.com/scl/fi/ms4yskwemwv9l7bhsray7/Cars.zip?rlkey=8dexth8n2g3i3j9e587ovp3i8&st=owbiere0&dl=0
    - wget https://www.dropbox.com/scl/fi/8znq5dgz60rn5gpkjmcls/DTD.zip?rlkey=krb6wyer3kbqldpmhny6ailly&st=1t4jj6wd&dl=0
    - wget https://www.dropbox.com/scl/fi/scpfdonhp7w3or9uokzka/eurosat.zip?rlkey=d5nva74i2oblbme9s64khi8go&st=sepex448&dl=0
    - wget https://www.dropbox.com/scl/fi/zawp0h8hp7cksy16fcue0/Flower102.zip?rlkey=1fyrz1irzad7ew5ikdsssajtm&st=t0nnb8pi&dl=0
    - wget https://www.dropbox.com/scl/fi/2xuu6z3cv29dj0hob6kum/Pets.zip?rlkey=of2b3pify7q8uozw73jqs5z27&st=iei5oz63&dl=0
    - wget https://www.dropbox.com/scl/fi/10nypnvwiaw0qwzui4s4j/UCF101.zip?rlkey=15gt8s82wdo0s4ty43mrtjlr8&st=fud6xcuc&dl=0
    
    ### language: bash
    set -e
    echo "Downloading dataset..."
    ```

---
+++ downloading_datasets/run_6.sh
@@ -0,0 +1,13 @@
+
+# vit_l_14_datacomp_1b
+cd /leonardo_work/EUHPC_R04_192/fmohamma/Adversarial_Robust_Clip/atpt_data
+mkdir vit_l_14_datacomp_1b
+
+wget https://www.dropbox.com/scl/fi/cnn70dahaftyyv2n78w1j/Aircraft.zip?rlkey=e9y0g3d8cs5c5rinphjqt41aw&st=rw0gcj1h&dl=0
+wget https://www.dropbox.com/scl/fi/s1917wxk72afma8lg7vdq/Caltech101.zip?rlkey=hk0gmdzqpxnsp0hz2mk65byvt&st=97m7ycrh&dl=0
+wget https://www.dropbox.com/scl/fi/ms4yskwemwv9l7bhsray7/Cars.zip?rlkey=8dexth8n2g3i3j9e587ovp3i8&st=owbiere0&dl=0
+wget https://www.dropbox.com/scl/fi/8znq5dgz60rn5gpkjmcls/DTD.zip?rlkey=krb6wyer3kbqldpmhny6ailly&st=1t4jj6wd&dl=0
+wget https://www.dropbox.com/scl/fi/scpfdonhp7w3or9uokzka/eurosat.zip?rlkey=d5nva74i2oblbme9s64khi8go&st=sepex448&dl=0
+wget https://www.dropbox.com/scl/fi/zawp0h8hp7cksy16fcue0/Flower102.zip?rlkey=1fyrz1irzad7ew5ikdsssajtm&st=t0nnb8pi&dl=0
+wget https://www.dropbox.com/scl/fi/2xuu6z3cv29dj0hob6kum/Pets.zip?rlkey=of2b3pify7q8uozw73jqs5z27&st=iei5oz63&dl=0
+wget https://www.dropbox.com/scl/fi/10nypnvwiaw0qwzui4s4j/UCF101.zip?rlkey=15gt8s82wdo0s4ty43mrtjlr8&st=fud6xcuc&dl=0
