0b105ffbbcaf771c341a868731a51e427638a2cd
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-07-09 17:16:39 +0400

    Update `create_adv_1.sh` to include new job scheduling commands with altered dataset paths and configurations, and comment out redundant commands for improved clarity.

--- create_adv_1.sh
+++ create_adv_1.sh
@@ -8,5 +8,8 @@
 #
 #
 #bash gen_adv.sh /mnt/nvme0n1/Dataset/muzammal/downstream_datasets 2 0 delta_clip_l14_224 4.0 100 1 0.1 20 0.01 /mnt/nvme0n1/Dataset/muzammal/atpt_results
-bash gen_adv.sh /mnt/nvme0n1/Dataset/muzammal/downstream_datasets 2 0 vit_l_14_datacomp_1b 4.0 100 1 0.1 20 0.01 /mnt/nvme0n1/Dataset/muzammal/atpt_results
-bash gen_adv.sh /mnt/nvme0n1/Dataset/muzammal/downstream_datasets 2 0 vit_l_14_datacomp_1b 1.0 100 1 0.1 20 0.01 /mnt/nvme0n1/Dataset/muzammal/atpt_results
+#bash gen_adv.sh /mnt/nvme0n1/Dataset/muzammal/downstream_datasets 2 0 vit_l_14_datacomp_1b 4.0 100 1 0.1 20 0.01 /mnt/nvme0n1/Dataset/muzammal/atpt_results
+#bash gen_adv.sh /mnt/nvme0n1/Dataset/muzammal/downstream_datasets 2 0 vit_l_14_datacomp_1b 1.0 100 1 0.1 20 0.01 /mnt/nvme0n1/Dataset/muzammal/atpt_results
+
+bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 RN50 1.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results true
+bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 1.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results true
