c5ba8d8117b5a62269dd7c23f07569f7eb1b70f1
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2025-07-17 09:17:07 +0400

    update

--- create_image_only_adv_cscc.sh
+++ create_image_only_adv_cscc.sh
@@ -16,28 +16,41 @@ wait_for_jobs() {
 }
 
 
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
+#wait_for_jobs
+#
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
+#wait_for_jobs
+#
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
+#wait_for_jobs
+#
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
+#sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
+#wait_for_jobs
+
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
 wait_for_jobs
 
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 ViT-L/14 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
-wait_for_jobs
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
+sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
 
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
-wait_for_jobs
-
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 fare4 4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
 wait_for_jobs
 
 sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 delta_clip_l14_224  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
@@ -50,15 +63,3 @@ sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 de
 sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 delta_clip_l14_224  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
 sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 delta_clip_l14_224  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
 sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 delta_clip_l14_224  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
-wait_for_jobs
-
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true cars"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true dtd"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true caltech101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true pets"
-wait_for_jobs
-
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true ucf101"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true eurosat"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true flower102"
-sbatch run.sh "bash gen_adv.sh /l/users/hashmat.malik/downstream_datasets 0 4 vit_l_14_datacomp_1b  4.0 100 1 0.1 20 0.01 /l/users/hashmat.malik/atpt_results /l/users/hashmat.malik/atpt_results  true  noisy_anchor 1 1.2 0.18 0.0 true aircraft"
