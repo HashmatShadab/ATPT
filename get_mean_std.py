Fix tensor dimension order in transformer feature extraction, add `torch.no_grad` wrapper in `get_mean_std.py` to prevent gradient computation.

get_mean_std.py
@@ -622,7 +622,8 @@ def test_time_adapt_eval(val_loader, model, model_state, optimizer, optim_state,
 
 
         # Clean Samples
-        cls_token, features_after_attention, features_after_mlp = model(images)
+        with torch.no_grad():
+            cls_token, features_after_attention, features_after_mlp = model(images)
         # cls_token shape : (B, D) Batch size, D is the dimension of the CLIP model
         # features_after_attention is a list of features for each layer: each tensor is of shape (N,B,D) N is the number of tokens, B is the batch size, D is the dimension of the CLIP model
         # features_after_mlp is a list of features for each layer: each tensor is of shape N,B,D) N is the number of tokens, B is the batch size, D is the dimension of the CLIP model

open_clip/transformer.py
@@ -563,8 +563,8 @@ class Transformer(nn.Module):
                 else:
                     x, attn_output, mlp_output = r(x, attn_mask=attn_mask, return_features=True)
                 
-                features_after_attention.append(attn_output)
-                features_after_mlp.append(mlp_output)
+                features_after_attention.append(attn_output.permute(1, 0, 2))
+                features_after_mlp.append(mlp_output.permute(1, 0, 2))
             
             if not self.batch_first:
                 x = x.transpose(0, 1)    # LND -> NLD