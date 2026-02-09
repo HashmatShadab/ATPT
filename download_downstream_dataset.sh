9446a5ee847adb3f284f0e7e11bde0cbe962c929
Naufal Suryanto <naufalsuryanto@gmail.com>
2026-02-09 03:37:45 +0100

    add

---
+++ download_downstream_dataset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Downstream
+wget https://www.dropbox.com/scl/fi/qj8pbn4hkd9usavwpg9vb/Caltech101.zip?rlkey=uh77mgw1ft1us86nadijjn4tg&st=3s5b3emo&dl=0
+wget https://www.dropbox.com/scl/fi/f3xl2tu8aklf32f5vgzl0/dtd.zip?rlkey=jieh0l5462k5fkftlng441c7c&st=k1f3qhjl&dl=0
+wget https://www.dropbox.com/scl/fi/n8hgfnac3rbzxnqrv9tak/eurosat.zip?rlkey=6rxp349n3ua0d2grnwt431xfo&st=bpt9w2lk&dl=0
+wget https://www.dropbox.com/scl/fi/28oq05aipeczfnffscz1j/fgvc_aircraft.zip?rlkey=uysn7zqjx65h6f16nnj22nnme&st=llupb9bl&dl=0
+wget https://www.dropbox.com/scl/fi/y2o7bip0a1dymuwm90woe/Flower102.zip?rlkey=wthvbjmaypwgfzdy9ekhfvvpi&st=bdp0beyo&dl=0
+wget https://www.dropbox.com/scl/fi/mcg4vc6tybe7bpyzkpm8p/imagenet-adversarial.zip?rlkey=susk8mhryc87zs6hm8t7xb8kx&st=2yrepk5q&dl=0
+wget https://www.dropbox.com/scl/fi/saeij2s22zessuibjyq4o/imagenet-rendition.zip?rlkey=qlacmx19ot0sln0hatpgqxejt&st=y6khy8r4&dl=0
+wget https://www.dropbox.com/scl/fi/ei4xx9e4gdwj5vgoiyf95/imagenet-sketch.zip?rlkey=23pw7ybr6mvkt4miosrvbhlny&st=53e8egdb&dl=0
+wget https://www.dropbox.com/scl/fi/3rnwlbhyns7h0l9xl6dtv/imagenet.zip?rlkey=r5n7nye7tvdm1xv0dqhrl0dqp&st=hvw2p7gu&dl=0
+wget https://www.dropbox.com/scl/fi/g9teq2z1idqd09a6mteq4/imagenetv2.zip?rlkey=wo3bo3fufyytrcto3lr6w9gdq&st=pe9ng3du&dl=0
+wget https://www.dropbox.com/scl/fi/y2x8eyvnjuhbshvg7fuyp/OxfordPets.zip?rlkey=wk9o1qiwvd5jwx0ldaha4v3zl&st=2h07ceok&dl=0
+wget https://www.dropbox.com/scl/fi/jakdz3y30e55g1wfx7ma5/stanford_cars.zip?rlkey=yup6u2o0yomu68uottx367cpm&st=w0bf0e7b&dl=0
+wget https://www.dropbox.com/scl/fi/rrhnpjgsvldnwiws2ubrh/UCF101.zip?rlkey=mvg0b9y9w82a1zeohtlvfa469&st=uk8vr07m&dl=0
