36853041a6a3b67b0d9ae06bb1011cf67e05a9d8
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2026-03-02 07:11:02 +0400

    Refactor `adv_gen_analysis.py` to enable saving vision features for clean, adversarial, and noise-altered images, streamline path handling for feature directories, remove unused variables, and improve file export consistency.

---
+++ create_vis_features.sh
@@ -0,0 +1,16 @@
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all false 0 uniform 1.0 0.24 normal 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    0.0 0  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all false 0 uniform 1.0 0.24 normal 10
+#
+#
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 1.0 0.24 normal 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 2.0 0.24 normal 10
+##bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 4.0 0.24 normal 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 8.0 0.24 normal 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 16.0 0.24 normal 10
+##bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 uniform 24.0 0.24 normal 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 32.0 0.005 noisy 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 40.0 0.01 noisy 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 48.0 0.02 noisy 10
+#bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 48.0 0.03 noisy 10
+bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 48.0 0.06 noisy 10
+bash gen_vision_features.sh ../atpt_data/downstream_datasets 0 4 vit_l_14_datacomp_1b    4.0 100  1 0.1 20 0.01 ../atpt_data ./tsne false noisy_anchor 10 1.2 0.18 0.0 false prm all true 0 gaussian 48.0 0.09 noisy 10
